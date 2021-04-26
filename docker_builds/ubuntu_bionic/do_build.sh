78c7ef0242ddda8e3f96322c5e55e697548ad40a
Ryzerth <whatsthetgeek@gmail.com>
2021-04-26 15:34:32 +0200

    Fixed more bionic stuff 2

--- docker_builds/ubuntu_bionic/do_build.sh
+++ docker_builds/ubuntu_bionic/do_build.sh
@@ -6,6 +6,9 @@ cd /root
 apt update
 apt install -y dpkg-dev make gcc-8 g++-8 cmake git libfftw3-dev libglfw3-dev libglew-dev libvolk1-dev libsoapysdr-dev libairspyhf-dev libairspy-dev libiio-dev libad9361-dev librtaudio-dev libhackrf-dev librtlsdr-dev
 
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 700 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+
 git clone https://github.com/AlexandreRouma/SDRPlusPlus
 
 cd SDRPlusPlus
