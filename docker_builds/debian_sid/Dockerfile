48ae57ad2def9df05e5c89ffad27d8c4bb658410
Ryzerth <whatsthetgeek@gmail.com>
2021-04-26 17:41:03 +0200

    Added CI for debian sid

---
+++ docker_builds/debian_sid/Dockerfile
@@ -0,0 +1,4 @@
+FROM debian:sid
+ENV DEBIAN_FRONTEND=noninteractive 
+COPY do_build.sh /root
+RUN chmod +x /root/do_build.sh
---
+++ docker_builds/debian_sid/do_build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+cd /root
+
+
+apt update
+apt install -y build-essential cmake git libfftw3-dev libglfw3-dev libglew-dev libvolk2-dev libsoapysdr-dev libairspyhf-dev libairspy-dev libiio-dev libad9361-dev librtaudio-dev libhackrf-dev librtlsdr-dev
+
+git clone https://github.com/AlexandreRouma/SDRPlusPlus
+
+cd SDRPlusPlus
+mkdir build
+cd build
+cmake ..
+make -j2
+
+cd ..
+sh make_debian_package.sh ./build
