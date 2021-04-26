b9a41c83bfe4d88f40ea37f5d6df63b178b8ec53
Ryzerth <whatsthetgeek@gmail.com>
2021-04-26 23:34:24 +0200

    Added sdrplay source to CI

--- docker_builds/debian_buster/do_build.sh
+++ docker_builds/debian_buster/do_build.sh
@@ -2,16 +2,23 @@
 set -e
 cd /root
 
-
+# Install dependencies and tools
 apt update
-apt install -y build-essential cmake git libfftw3-dev libglfw3-dev libglew-dev libvolk1-dev libsoapysdr-dev libairspyhf-dev libairspy-dev libiio-dev libad9361-dev librtaudio-dev libhackrf-dev librtlsdr-dev
+apt install -y build-essential cmake git libfftw3-dev libglfw3-dev libglew-dev libvolk1-dev libsoapysdr-dev libairspyhf-dev libairspy-dev libiio-dev libad9361-dev librtaudio-dev libhackrf-dev librtlsdr-dev p7zip-full wget
+
+# Install SDRPlay libraries
+wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.07.1.run
+7z x ./SDRplay_RSP_API-Linux-3.07.1.run
+7z x ./SDRplay_RSP_API-Linux-3.07.1
+cp x86_64/libsdrplay_api.so.3.07 /usr/lib/libsdrplay_api.so
+cp inc/* /usr/include/
 
 git clone https://github.com/AlexandreRouma/SDRPlusPlus
 
 cd SDRPlusPlus
 mkdir build
 cd build
-cmake ..
+cmake .. -DOPT_BUILD_SDRPLAY_SOURCE=ON
 make -j2
 
 cd ..
