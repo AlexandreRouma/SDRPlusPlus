f0b2d80ba7f2a5c13fc46f61dfc49062ea2d400b
Marvin Sinister <marvin.sinister@gmail.com>
2021-07-03 13:35:42 +0200

    docker build improvements

--- docker_builds/ubuntu_focal/do_build.sh
+++ docker_builds/ubuntu_focal/do_build.sh
@@ -4,7 +4,7 @@ cd /root
 
 # Install dependencies and tools
 apt update
-apt install -y build-essential cmake git libfftw3-dev libglfw3-dev libglew-dev libvolk2-dev libsoapysdr-dev libairspyhf-dev libairspy-dev libiio-dev libad9361-dev librtaudio-dev libhackrf-dev librtlsdr-dev libbladerf-dev liblimesuite-dev p7zip-full wget
+apt full-upgrade -y
 
 # Install SDRPlay libraries
 wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.07.1.run
@@ -12,8 +12,6 @@ wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.07.1.run
 7z x ./SDRplay_RSP_API-Linux-3.07.1
 cp x86_64/libsdrplay_api.so.3.07 /usr/lib/libsdrplay_api.so
 cp inc/* /usr/include/
-
-git clone https://github.com/AlexandreRouma/SDRPlusPlus
 
 cd SDRPlusPlus
 mkdir build
