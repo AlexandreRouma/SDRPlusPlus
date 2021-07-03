5a21b07269e665a20d8a5507250154196db64be8
Marvin Sinister <marvin.sinister@gmail.com>
2021-07-03 14:46:55 +0200

    revert package installation changes

--- docker_builds/debian_buster/do_build.sh
+++ docker_builds/debian_buster/do_build.sh
@@ -4,7 +4,7 @@ cd /root
 
 # Install dependencies and tools
 apt update
-apt full-upgrade -y
+apt install -y build-essential cmake git libfftw3-dev libglfw3-dev libglew-dev libvolk1-dev libsoapysdr-dev libairspyhf-dev libairspy-dev libiio-dev libad9361-dev librtaudio-dev libhackrf-dev librtlsdr-dev libbladerf-dev liblimesuite-dev p7zip-full wget
 
 # Install SDRPlay libraries
 wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.07.1.run
