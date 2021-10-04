9555dda8a499218853f6ee57f2db678d20f2949b
AlexandreRouma <alexandre.rouma@gmail.com>
2021-10-04 03:31:13 +0200

    More cmake cleanup

--- decoder_modules/falcon9_decoder/src/libcorrect/correct/reed-solomon/encode.h
+++
@@ -1,3 +0,0 @@
-#include "correct/reed-solomon.h"
-#include "correct/reed-solomon/field.h"
-#include "correct/reed-solomon/polynomial.h"
--- docker_builds/debian_buster/do_build.sh
+++ docker_builds/debian_buster/do_build.sh
@@ -19,7 +19,7 @@ cd SDRPlusPlus
 mkdir build
 cd build
 cmake .. -DOPT_BUILD_SDRPLAY_SOURCE=ON -DOPT_BUILD_BLADERF_SOURCE=OFF -DOPT_BUILD_LIMESDR_SOURCE=ON -DOPT_BUILD_NEW_PORTAUDIO_SINK=ON -DOPT_BUILD_M17_DECODER=ON
-make -j2
+make VERBOSE=1 -j2
 
 cd ..
 sh make_debian_package.sh ./build 'libfftw3-dev, libglfw3-dev, libglew-dev, libvolk1-dev, librtaudio-dev'
