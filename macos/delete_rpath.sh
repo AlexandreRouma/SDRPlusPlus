49cf6944f0ecf52596c552fe4cbe67528b128861
AlexandreRouma <alexandre.rouma@gmail.com>
2021-11-15 18:26:33 +0100

    Added missing file

---
+++ macos/delete_rpath.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+# Options
+RPATH_NAME=$1
+EXEC=$2
+
+# Function to keep only the second arg
+get_second_arg() {
+    echo $2
+}
+
+# Get current rpath
+WANTED_RPATH=$(get_second_arg $(otool -l $EXEC | grep $RPATH_NAME | grep path))
+
+install_name_tool -delete_rpath $WANTED_RPATH $EXEC
