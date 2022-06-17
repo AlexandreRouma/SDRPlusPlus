ccc57cddc7069753e4e5c14998b6adec0be03cb0
AlexandreRouma <alexandre.rouma@gmail.com>
2022-06-17 19:43:16 +0200

    hopefully the final bugfix

--- make_windows_package.ps1
+++ make_windows_package.ps1
@@ -56,10 +56,10 @@ cp $build_dir/sink_modules/network_sink/Release/network_sink.dll sdrpp_windows_x
 
 
 # Copy decoder modules
-cp $build_dir/decoder_modules/m17_decoder/Release/m17_decoder.dll sdrpp_windows_x64/modules/
-cp "C:/Program Files/codec2/lib/libcodec2.dll" sdrpp_windows_x64/
+# cp $build_dir/decoder_modules/m17_decoder/Release/m17_decoder.dll sdrpp_windows_x64/modules/
+# cp "C:/Program Files/codec2/lib/libcodec2.dll" sdrpp_windows_x64/
 
-cp $build_dir/decoder_modules/meteor_demodulator/Release/meteor_demodulator.dll sdrpp_windows_x64/modules/
+# cp $build_dir/decoder_modules/meteor_demodulator/Release/meteor_demodulator.dll sdrpp_windows_x64/modules/
 cp $build_dir/decoder_modules/radio/Release/radio.dll sdrpp_windows_x64/modules/
 
 
