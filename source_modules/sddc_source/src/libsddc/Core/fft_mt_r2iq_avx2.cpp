d3e9ebef72570c0d9b16565d37e124adf488e523
AlexandreRouma <alexandre.rouma@gmail.com>
2022-07-09 15:54:39 +0200

    removed broken SDDC source

--- source_modules/sddc_source/src/libsddc/Core/fft_mt_r2iq_avx2.cpp
+++
@@ -1,9 +0,0 @@
-#include "fft_mt_r2iq.h"
-#include "sddc_config.h"
-#include "fftw3.h"
-#include "RadioHandler.h"
-
-void * fft_mt_r2iq::r2iqThreadf_avx2(r2iqThreadArg *th)
-{
-    #include "fft_mt_r2iq_impl.hpp"
-}
--- source_modules/sddc_source/src/libsddc/Core/fir.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-int KaiserWindow(int num_taps, float Astop, float normFpass, float normFstop, float * Coef);
