93e985ab54f9fee430d82398978944e18656c01e
AlexandreRouma <alexandre.rouma@gmail.com>
2023-02-16 07:59:01 +0100

    bugfix

--- sdrpp_module.cmake
+++ sdrpp_module.cmake
@@ -2,8 +2,8 @@
 if (NOT SDRPP_CORE_ROOT)
     set(SDRPP_CORE_ROOT "@SDRPP_CORE_ROOT@")
 endif ()
-if (NOT SDRPP_MODULE_COMPILER_ARGS)
-    set(SDRPP_MODULE_COMPILER_ARGS @SDRPP_MODULE_COMPILER_ARGS@)
+if (NOT SDRPP_MODULE_COMPILER_FLAGS)
+    set(SDRPP_MODULE_COMPILER_FLAGS @SDRPP_MODULE_COMPILER_FLAGS@)
 endif ()
 
 # Created shared lib and link to core
@@ -13,7 +13,7 @@ target_include_directories(${PROJECT_NAME} PRIVATE "${SDRPP_CORE_ROOT}/src/")
 set_target_properties(${PROJECT_NAME} PROPERTIES PREFIX "")
 
 # Set compile arguments
-target_compile_options(${PROJECT_NAME} PRIVATE ${SDRPP_MODULE_COMPILER_ARGS})
+target_compile_options(${PROJECT_NAME} PRIVATE ${SDRPP_MODULE_COMPILER_FLAGS})
 
 # Install directives
 install(TARGETS ${PROJECT_NAME} DESTINATION lib/sdrpp/plugins)
