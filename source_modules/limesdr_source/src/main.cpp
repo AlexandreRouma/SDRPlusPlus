ae1fd87f0280e089c58b6a1b48881e6da449ee85
Sergey Sharybin <sergey.vfx@gmail.com>
2023-03-14 21:37:22 +0100

    Fix crash starting LimeSDR source without device
    This change fixes crash when a LimeSDR source is started without
    an actual device selected.

--- source_modules/limesdr_source/src/main.cpp
+++ source_modules/limesdr_source/src/main.cpp
@@ -319,6 +319,7 @@ private:
     static void start(void* ctx) {
         LimeSDRSourceModule* _this = (LimeSDRSourceModule*)ctx;
         if (_this->running) { return; }
+        if (_this->devCount == 0) { return; }
 
         // Open device
         _this->openDev = NULL;
