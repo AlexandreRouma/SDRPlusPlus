5ddb00064ed7f003325dbac0e2c0781941d81007
ericek111 <erik.brocko@letemsvetemapplem.eu>
2023-05-14 16:08:10 +0200

    add missing includes

--- core/src/gui/smgui.h
+++ core/src/gui/smgui.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdint.h>
 #include <imgui.h>
+#include <stdint.h>
 #include <string>
 #include <vector>
 #include <map>
--- core/src/utils/net.h
+++ core/src/utils/net.h
@@ -4,6 +4,7 @@
 #include <mutex>
 #include <memory>
 #include <map>
+#include <stdexcept>
 
 #ifdef _WIN32
 #include <WinSock2.h>
