32c580ba57fd9c627c9c11d35bdfd5757103d1b6
AlexandreRouma <alexandre.rouma@gmail.com>
2023-05-24 02:47:04 +0200

    another fix

--- docker_builds/debian_buster/do_build.sh
+++ docker_builds/debian_buster/do_build.sh
@@ -6,7 +6,7 @@ cd /root
 apt update
 apt install -y build-essential cmake git libfftw3-dev libglfw3-dev libvolk1-dev libzstd-dev libsoapysdr-dev libairspyhf-dev libairspy-dev \
             libiio-dev libad9361-dev librtaudio-dev libhackrf-dev librtlsdr-dev libbladerf-dev liblimesuite-dev p7zip-full wget portaudio19-dev \
-            libcodec2-dev
+            libcodec2-dev autoconf
 
 # Install SDRPlay libraries
 wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.07.1.run
