5acdab0d22f612507637e4ba759970ad7dd3c6f2
AlexandreRouma <alexandre.rouma@gmail.com>
2023-06-10 21:48:02 +0200

    Instantiated the perseus source by default and perseus on raspberrypi

--- core/src/core.cpp
+++ core/src/core.cpp
@@ -179,6 +179,8 @@ int sdrpp_main(int argc, char* argv[]) {
     defConfig["moduleInstances"]["LimeSDR Source"]["enabled"] = true;
     defConfig["moduleInstances"]["PlutoSDR Source"]["module"] = "plutosdr_source";
     defConfig["moduleInstances"]["PlutoSDR Source"]["enabled"] = true;
+    defConfig["moduleInstances"]["PerseusSDR Source"]["module"] = "perseus_source";
+    defConfig["moduleInstances"]["PerseusSDR Source"]["enabled"] = true;
     defConfig["moduleInstances"]["RFspace Source"]["module"] = "rfspace_source";
     defConfig["moduleInstances"]["RFspace Source"]["enabled"] = true;
     defConfig["moduleInstances"]["RTL-SDR Source"]["module"] = "rtl_sdr_source";
