5e195a0d439486ad3cc1890616de15c7471728d7
AlexandreRouma <alexandre.rouma@gmail.com>
2024-01-19 20:53:25 +0100

    testing something for macos (do not use this CI build)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <core.h>
 #include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]) {
+extern char **environ;
+
+int main(int argc, char* argv[], char* envp[]) {
+    FILE* dump = fopen("/Users/ryzerth/dump.txt", "wb");
+
+    char buf[1024];
+    fprintf(dump, "Working directory: %s\n\n", getcwd(buf, 1023));
+    
+    fprintf(dump, "Arguments:\n");
+    fprintf(dump, "----------\n");
+    for (int i = 0; i < argc; i++) {
+        fprintf(dump, "%d: '%s'\n", i, argv[i]);
+    }
+    fprintf(dump, "\n");
+
+    fprintf(dump, "Environment Variables:\n");
+    fprintf(dump, "----------\n");
+    for (char** env = environ; *env; env++) {
+        fprintf(dump, "%s\n", *env);
+    }
+    fprintf(dump, "\n");
+
+    fclose(dump);
+
     return sdrpp_main(argc, argv);
 }
