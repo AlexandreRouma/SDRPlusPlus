0144d8e8ce599449d66545c25e43463fa5ef9b96
AlexandreRouma <alexandre.rouma@gmail.com>
2024-01-30 18:08:17 +0100

    resolve conflict in radio

--- decoder_modules/radio/src/demodulators/wfm.h
+++ decoder_modules/radio/src/demodulators/wfm.h
@@ -16,8 +16,8 @@ namespace demod {
     public:
         WFM() {}
 
-        WFM(std::string name, ConfigManager* config, dsp::stream<dsp::complex_t>* input, double bandwidth) {
-            init(name, config, input, bandwidth);
+        WFM(std::string name, ConfigManager* config, dsp::stream<dsp::complex_t>* input, double bandwidth, double audioSR) : diag(0.5, 4096) {
+            init(name, config, input, bandwidth, audioSR);
         }
 
         ~WFM() {
