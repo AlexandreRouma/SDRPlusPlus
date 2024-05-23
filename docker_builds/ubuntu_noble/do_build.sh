733dc5572325acf16d51bfd49d3c0e98a3da2133
AlexandreRouma <alexandre.rouma@gmail.com>
2024-05-24 01:38:24 +0200

    added missing files

---
+++ docker_builds/ubuntu_noble/do_build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+cd /root
+
+# Install dependencies and tools
+apt update
+apt install -y build-essential cmake git libfftw3-dev libglfw3-dev libvolk-dev libzstd-dev libairspyhf-dev libairspy-dev \
+            libiio-dev libad9361-dev librtaudio-dev libhackrf-dev librtlsdr-dev libbladerf-dev liblimesuite-dev p7zip-full wget portaudio19-dev \
+            libcodec2-dev autoconf libtool xxd
+
+# Install SDRPlay libraries
+wget https://www.sdrplay.com/software/SDRplay_RSP_API-Linux-3.14.0.run
+7z x ./SDRplay_RSP_API-Linux-3.14.0.run
+7z x ./SDRplay_RSP_API-Linux-3.14.0
+cp x86_64/libsdrplay_api.so.3.14 /usr/lib/libsdrplay_api.so
+cp inc/* /usr/include/
+
+# Install libperseus
+git clone https://github.com/Microtelecom/libperseus-sdr
+cd libperseus-sdr
+autoreconf -i
+./configure
+make
+make install
+ldconfig
+cd ..
+
+cd SDRPlusPlus
+mkdir build
+cd build
+cmake .. -DOPT_BUILD_BLADERF_SOURCE=ON -DOPT_BUILD_LIMESDR_SOURCE=ON -DOPT_BUILD_SDRPLAY_SOURCE=ON -DOPT_BUILD_NEW_PORTAUDIO_SINK=ON -DOPT_BUILD_M17_DECODER=ON -DOPT_BUILD_PERSEUS_SOURCE=ON
+make VERBOSE=1 -j2
+
+cd ..
+sh make_debian_package.sh ./build 'libfftw3-dev, libglfw3-dev, libvolk-dev, librtaudio-dev, libzstd-dev'
