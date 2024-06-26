e613087e97adf929b920d67cea6792898869170a
AlexandreRouma <alexandre.rouma@gmail.com>
2024-06-26 12:51:13 +0200

    another mutex test

--- min_broken/main.cpp
+++ min_broken/main.cpp
@@ -1,26 +1,8 @@
-#include <stdio.h>
 #include <mutex>
 
-class TestClass {
-public:
-    TestClass() {
-        std::lock_guard<std::recursive_mutex> lck(mtx);
-        value = 42;
-    }
-
-    int getValue() {
-        std::lock_guard<std::recursive_mutex> lck(mtx);
-        return value;
-    }
-
-private:
-    std::recursive_mutex mtx;
-    int value = 0;
-};
-
-TestClass test;
+std::recursive_mutex mtx;
 
 int main() {
-    printf("Value: %d\n", test.getValue());
+    std::lock_guard<std::recursive_mutex> lck(mtx);
     return 0;
 }
