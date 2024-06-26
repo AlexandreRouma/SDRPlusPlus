c4086f57198ebce730103e9acb8cf60cf25260a3
AlexandreRouma <alexandre.rouma@gmail.com>
2024-06-26 13:29:05 +0200

    add test on github

--- min_broken/main.cpp
+++ min_broken/main.cpp
@@ -1,7 +1,12 @@
+#include <stdio.h>
 #include <mutex>
 
+std::recursive_mutex mtx;
+
 int main() {
-    std::recursive_mutex mtx;
     std::lock_guard<std::recursive_mutex> lck(mtx);
+
+    printf("Works just fine!\n");
+
     return 0;
 }
