d5bc0fa3c399f0585bcc3617c8e1105094644488
AlexandreRouma <alexandre.rouma@gmail.com>
2024-08-25 20:42:20 +0200

    switched cmakelist to use gnurinstalldir instead of hardcoded paths

--- sdrpp_module.cmake
+++ sdrpp_module.cmake
@@ -16,4 +16,4 @@ set_target_properties(${PROJECT_NAME} PROPERTIES PREFIX "")
 target_compile_options(${PROJECT_NAME} PRIVATE ${SDRPP_MODULE_COMPILER_FLAGS})
 
 # Install directives
-install(TARGETS ${PROJECT_NAME} DESTINATION lib/sdrpp/plugins)
+install(TARGETS ${PROJECT_NAME} DESTINATION ${CMAKE_INSTALL_LIBDIR}/sdrpp/plugins)
